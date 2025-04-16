9be9775af626f20ba44cde2a8397c052e3ba4639
Gabriele Filomena <gfilo@liverpool.ac.uk>
2025-04-16 12:14:49 +0100

    Update RouteChoicePars.java

--- src/main/java/pedSim/parameters/RouteChoicePars.java
+++ src/main/java/pedSim/parameters/RouteChoicePars.java
@@ -12,23 +12,26 @@ public class RouteChoicePars {
 	public static double thresholdTurn = 45;
 	public static int MIN_WALKED_ROUTES_SIZE = 5;
 
-	// Distance between Origin and Destination
-	public static double minTripDistance = 700;
-	public static double avgTripDistance = 1800;
-	public static double maxTripDistance = 2500;
-	public static double maxTripsPerDay = 6;
+	// Euclidean Distance between Origin and Destination -> Walking distance will be
+	// probably EuclideanDistance*1.25
+	public static double avgTripDistance = 1300;
+	public static double minTripDistance;
+	public static double maxTripDistance;
 
 	public Integer[] originsTmp = {};
 	public static Integer[] destinationsTmp = {};
 	public static Integer[] cityCentreRegionsID = { 0, 2, 13, 33 };
 
+	public static double naturalBarriers = 0.30;
+	public static double naturalBarriersSD = 0.15;
+
 	// Landmark Integration
 	public static double distanceNodeBuilding = 40.0;
 	public static double salientNodesPercentile = 0.90; // Threshold Percentile to identify salient nodes
 
-	public static double naturalBarriers = 0.49;
-	public static double naturalBarriersSD = 0.21;
-	public static double severingBarriers = 0.53;
-	public static double severingBarriersSD = 0.29;
+	public static void setMinMaxTripDistance() {
+		minTripDistance = avgTripDistance * 0.50;
+		maxTripDistance = avgTripDistance * 1.50;
+	}
 
 }
