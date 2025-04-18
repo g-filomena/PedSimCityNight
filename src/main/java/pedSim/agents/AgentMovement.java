8510791764433a1333125e374a4060f61c6c6fff
Gabriele Filomena <gfilo@liverpool.ac.uk>
2025-04-18 18:29:51 +0100

    Update AgentMovement.java

--- src/main/java/pedSim/agents/AgentMovement.java
+++ src/main/java/pedSim/agents/AgentMovement.java
@@ -3,10 +3,12 @@ package pedSim.agents;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Random;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+import org.javatuples.Pair;
 import org.locationtech.jts.geom.Coordinate;
 import org.locationtech.jts.geom.LineString;
 import org.locationtech.jts.linearref.LengthIndexedLine;
@@ -15,14 +17,18 @@ import org.locationtech.jts.planargraph.DirectedEdge;
 import pedSim.cognitiveMap.CommunityCognitiveMap;
 import pedSim.engine.PedSimCity;
 import pedSim.parameters.Pars;
-import pedSim.utilities.StringEnum.Vulnerable;
 import sim.graph.EdgeGraph;
 import sim.graph.Graph;
+import sim.graph.GraphUtils;
 import sim.graph.NodeGraph;
 import sim.routing.Astar;
-import sim.routing.AstarNight;
 import sim.routing.Route;
 
+/**
+ * The AgentMovement class is responsible for handling the movement of agents within the simulation. It manages the
+ * agent's movement along the path, including transitions between edges, speed adjustments, and the handling of various
+ * conditions such as night-time speed increase and vulnerable agent behaviour.
+ */
 public class AgentMovement {
 
 	// How much to move the agent by in each step()
@@ -52,6 +58,8 @@ public class AgentMovement {
 	private NodeGraph currentNode;
 	private Graph network;
 
+	private boolean avoidParksWater;
+
 	public AgentMovement(Agent agent) {
 		this.agent = agent;
 		this.state = agent.getState();
@@ -59,7 +67,9 @@ public class AgentMovement {
 	}
 
 	/**
-	 * Initialises the directedEdgesSequence (the path) for the agent.
+	 * Initialises the path (directed edges sequence) for the agent.
+	 * 
+	 * @param route The route that defines the path the agent should follow.
 	 */
 	public void initialisePath(Route route) {
 
@@ -76,12 +86,13 @@ public class AgentMovement {
 	}
 
 	/**
-	 * Sets the agent up to proceed along an edge.
-	 *
-	 * @param edge The EdgeGraph to traverse next.
+	 * Sets the agent up to proceed along a specified edge.
+	 * 
+	 * @param directedEdge The DirectedEdge to traverse next.
 	 */
 	void setupEdge(DirectedEdge directedEdge) {
 
+		avoidParksWater = false;
 		increaseSpeedAtNight = false; // removing potential increases
 		currentDirectedEdge = directedEdge;
 		currentEdge = (EdgeGraph) currentDirectedEdge.getEdge();
@@ -104,7 +115,7 @@ public class AgentMovement {
 	}
 
 	/**
-	 * Moves the agent along the computed route.
+	 * Moves the agent along the current path.
 	 */
 	protected void keepWalking() {
 
@@ -128,10 +139,9 @@ public class AgentMovement {
 	}
 
 	/**
-	 * Transitions to the next edge in the {@code directedEdgesSequence}.
-	 *
-	 * @param residualMove The amount of distance the agent can still travel this
-	 *                     step.
+	 * Transitions the agent to the next edge in the sequence if the agent has finished walking the current edge.
+	 * 
+	 * @param residualMove The remaining distance to travel on the current edge.
 	 */
 	void transitionToNextEdge(double residualMove) {
 
@@ -165,66 +175,117 @@ public class AgentMovement {
 	}
 
 	/**
-	 * Updates the agent's speed based on the move rate and link direction.
-	 * 
-	 * @param pedestrianDensity
+	 * Resets the agent's movement reach to the base move rate.
 	 */
-
 	private void resetReach() {
 		reach = Pars.moveRate;
 	}
 
+	/**
+	 * Increases the agent's movement reach based on the speed factor for night time.
+	 */
 	private void increaseReach() {
 		reach = reach + (Pars.moveRate * Pars.SPEED_INCREMENT_FACTOR);
 	}
 
+	/**
+	 * Updates the agent's reach if the agent is moving faster at night.
+	 */
 	private void updateReach() {
 		if (increaseSpeedAtNight)
 			increaseReach();
 	}
 
+	/**
+	 * Computes an alternative route for the agent to avoid dangerous or unsuitable edges, reusing a cached route if
+	 * available.
+	 */
 	private void computeAlternativeRoute() {
-
-		// get the one before the "bad" edge
 		NodeGraph currentNode = (NodeGraph) edgesWalkedSoFar.get(edgesWalkedSoFar.size() - 1).getToNode();
+		Pair<NodeGraph, NodeGraph> routeKey = Pair.with(currentNode, agent.destinationNode);
+		Map<Pair<NodeGraph, NodeGraph>, List<DirectedEdge>> cache = (agent.isVulnerable() || avoidParksWater)
+				? PedSimCity.altRoutesVulnerable
+				: PedSimCity.altRoutesNonVulnerable;
 
-		getEdgesToAvoid();
-		AstarNight aStarNight = new AstarNight();
-		Route alternativeRoute = aStarNight.astarRoute(currentNode, agent.destinationNode, network, edgesToAvoid,
-				agent);
+		// Check if a cached route already exists
+		if (cache.containsKey(routeKey)) {
+			resetPath(new ArrayList<>(cache.get(routeKey)));
+			originalRoute = false;
+			return;
+		}
+
+		setEdgesToAvoid();
+		Astar aStar = new Astar();
+		Route alternativeRoute = aStar.astarRoute(currentNode, agent.destinationNode, network, edgesToAvoid);
 
 		int iteration = 0;
 		while (alternativeRoute == null) {
-			if (iteration == 0)
-				edgesToAvoid.removeAll(CommunityCognitiveMap.edgesAlongWater);
-			else if (iteration == 1)
-				edgesToAvoid.removeAll(CommunityCognitiveMap.edgesWithinParks);
-			else if (iteration == 2)
-				edgesToAvoid.removeAll(CommunityCognitiveMap.nonLitNonPrimary);
-			alternativeRoute = Astar.astarRoute(currentNode, agent.destinationNode, CommunityCognitiveMap.getNetwork(),
+			switch (iteration) {
+			case 0 -> {
+				// Add secondary roads, still try avoiding non-lit and parks/water
+				edgesToAvoid.removeAll(CommunityCognitiveMap.getNeighbourhoodEdges());
+				edgesToAvoid.addAll(CommunityCognitiveMap.getEdgesNonLitNonCommunityKnown());
+				if (agent.isVulnerable() || avoidParksWater)
+					edgesToAvoid.addAll(CommunityCognitiveMap.getEdgesWithinParksOrAlongWater());
+			}
+			// give up park avoidance
+			case 1 -> edgesToAvoid.removeAll(CommunityCognitiveMap.getEdgesWithinParks());
+			// give up water avoidance
+			case 2 -> edgesToAvoid.removeAll(CommunityCognitiveMap.getEdgesAlongWater());
+			// give up non-lit avoidance
+			case 3 -> edgesToAvoid.removeAll(CommunityCognitiveMap.getEdgesNonLitNonCommunityKnown());
+			default -> {
+				edgesToAvoid.clear();
+			}
+			}
+			alternativeRoute = aStar.astarRoute(currentNode, agent.destinationNode, CommunityCognitiveMap.getNetwork(),
 					edgesToAvoid);
-			iteration += 1;
+			iteration++;
 		}
 
-		originalRoute = false;
+		// Cache and apply the new route
+		cache.put(routeKey, new ArrayList<>(alternativeRoute.directedEdgesSequence));
 		resetPath(alternativeRoute.directedEdgesSequence);
+		originalRoute = false;
 	}
 
-	private Set<EdgeGraph> getEdgesToAvoid() {
+	/**
+	 * Gets the set of edges that the agent should avoid during movement.
+	 * 
+	 * @return The set of edges to avoid.
+	 */
+	private void setEdgesToAvoid() {
 
-		edgesToAvoid.clear();
+		// the disregarded one
 		edgesToAvoid.add(currentEdge);
-		edgesToAvoid.addAll(CommunityCognitiveMap.nonLitNonPrimary);
 
-		if (isVulnerable()) {
-			edgesToAvoid.addAll(CommunityCognitiveMap.edgesAlongWater);
-			edgesToAvoid.addAll(CommunityCognitiveMap.edgesWithinParks);
+		// non-lit roads
+		edgesToAvoid.addAll(CommunityCognitiveMap.getEdgesNonLitNonCommunityKnown());
+
+		// for vulnerable:
+		if (agent.isVulnerable()) {
+			// 1) add everything,
+			edgesToAvoid.addAll(CommunityCognitiveMap.getCommunityNetwork().getEdges());
+			// 2) remove primary,
+			edgesToAvoid.removeAll(CommunityCognitiveMap.getCommunityKnownEdges());
+			// 3) remove known
+			Set<EdgeGraph> knownEdges = new HashSet<>(
+					GraphUtils.getEdgesFromEdgeIDs(agent.getCognitiveMap().getKnownEdges(), PedSimCity.edgesMap));
+			edgesToAvoid.removeAll(knownEdges);
 		}
+
+		// for vulnerable and non-vulnerable agents who do not feel like water and parks
+		if (agent.isVulnerable() || avoidParksWater)
+			edgesToAvoid.addAll(CommunityCognitiveMap.getEdgesWithinParksOrAlongWater());
 
 		edgesToAvoid.removeAll(agent.destinationNode.getEdges());
-		return edgesToAvoid;
 	}
 
+	/**
+	 * Adds an indexed segment to the indexed edge cache.
+	 *
+	 * @param edge The edge to add to the indexed edge cache.
+	 */
 	private void addIndexedSegment(EdgeGraph edge) {
 
 		LineString line = edge.getLine();
@@ -238,50 +299,72 @@ public class AgentMovement {
 		PedSimCity.indexedEdgeCache.put(currentDirectedEdge, new LengthIndexedLine(finalLine));
 	}
 
+	/**
+	 * Checks the light level of the current edge.
+	 */
 	private void checkLightLevel() {
 
 		// edge is lit but night
-		if (CommunityCognitiveMap.litEdges.contains(currentEdge))
+		if (CommunityCognitiveMap.getLitEdges().contains(currentEdge))
 			whenLit(currentEdge);
 		else
 			whenNonLit(currentEdge);
-
 	}
 
+	/**
+	 * Handles the case when the edge is lit.
+	 *
+	 * @param edge The edge to be approached.
+	 */
 	private void whenLit(EdgeGraph edge) {
 
-		// vulnerable avoid parks at night at planning phase.
+		// vulnerable agents avoid parks at night at planning phase.
 		if (isParkWaterNonVulnerable(edge))
 			whenParkWater(edge);
 		else
 			whenLitVulernable(edge);
 	}
 
+	/**
+	 * Checks if the edge is next to a park or water and the agent is non-vulnerable.
+	 *
+	 * @param edge The edge to check.
+	 * @return true if the edge is next to a park or water and non-vulnerable, false otherwise.
+	 */
 	private boolean isParkWaterNonVulnerable(EdgeGraph edge) {
-		return isEdgeNextToParkOrWater(edge) && !isVulnerable();
+		return isEdgeNextToParkOrWater(edge) && !agent.isVulnerable();
 	}
 
+	/**
+	 * Handles the case when the agent is approaching an edge in proximity or parks or water.
+	 *
+	 * @param edge The edge to be approached.
+	 */
 	private void whenParkWater(EdgeGraph edge) {
 		// vulnerable avoid parks at night at planning phase.
 		if (canReroute()) {
-			edgesToAvoid.addAll(CommunityCognitiveMap.edgesAlongWater);
-			edgesToAvoid.addAll(CommunityCognitiveMap.edgesWithinParks);
+			avoidParksWater = true;
 			computeAlternativeRoute();
 		} else
 			increaseSpeedAtNight = true;
 	}
 
+	/**
+	 * Handles the case when a vulnerable agent is approaching a lit edge.
+	 *
+	 * @param edge The lit edge to be approached.
+	 */
 	private void whenLitVulernable(EdgeGraph edge) {
 
-		// not known, not primary, not busy -> recompute
-		if (!isEdgeKnown(edge) && !isEdgePrimary(edge) && !isEdgeCrowded(edge)) {
+		// not known, not main road, not busy -> recompute
+		if (!isEdgeKnown(edge) && !isEdgeMainRoad(edge) && !isEdgeCrowded(edge)) {
 			if (canReroute())
 				computeAlternativeRoute();
 			else
 				increaseSpeedAtNight = true;
 		}
-		// not primary and not crowded -> reroute or increase speed
-		else if (!isEdgePrimary(edge) && !isEdgeCrowded(edge)) {
+		// not main road and not crowded -> reroute or increase speed
+		else if (!isEdgeMainRoad(edge) && !isEdgeCrowded(edge)) {
 			if (random.nextDouble() < 0.5 && canReroute())
 				computeAlternativeRoute();
 			else
@@ -289,14 +372,24 @@ public class AgentMovement {
 		}
 	}
 
+	/**
+	 * Determines what to do, based on the agent vulnerability, when an agent is approaching a non-lit edge.
+	 *
+	 * @param edge The lit edge to be approached.
+	 */
 	private void whenNonLit(EdgeGraph edge) {
 
-		if (isVulnerable())
+		if (agent.isVulnerable())
 			nonLitVulnerable(edge);
 		else
 			nonLit(edge);
 	}
 
+	/**
+	 * Handles the case when a non-vulnerable agent is approaching a non-lit edge.
+	 *
+	 * @param edge The lit edge to be approached.
+	 */
 	private void nonLit(EdgeGraph edge) {
 
 		if (isParkWaterNonVulnerable(edge)) {
@@ -306,18 +399,23 @@ public class AgentMovement {
 		// crowded -> ok
 		else if (isEdgeCrowded(edge))
 			return;
-		// not known, not primary, not crowded -> reroute or increase speed
-		else if (!isEdgeKnown(edge) && !isEdgePrimary(edge))
+		// not known, not main road, not crowded -> reroute or increase speed
+		else if (!isEdgeKnown(edge) && !isEdgeMainRoad(edge))
 			rerouteOrIncreaseSpeed();
-		// primary or known, not crowded -> ok
-		else if (isEdgePrimary(edge) || isEdgeKnown(edge))
+		// main road or known, not crowded -> ok
+		else if (isEdgeMainRoad(edge) || isEdgeKnown(edge))
 			return;
 	}
 
+	/**
+	 * Handles the case when a vulnerable agent is approaching a non-lit edge.
+	 *
+	 * @param edge The lit edge to be approached.
+	 */
 	private void nonLitVulnerable(EdgeGraph edge) {
 
-		// not known, not primary, not crowded -> reroute
-		if (!isEdgeKnown(edge) && !isEdgePrimary(edge) && !isEdgeCrowded(edge)) {
+		// not known, not main road, not crowded -> reroute
+		if (!isEdgeKnown(edge) && !isEdgeMainRoad(edge) && !isEdgeCrowded(edge)) {
 			if (canReroute())
 				computeAlternativeRoute();
 			else
@@ -326,14 +424,17 @@ public class AgentMovement {
 		// not known but crowded -> OK
 		else if (!isEdgeKnown(edge) && isEdgeCrowded(edge))
 			return;
-		// primary but not crowded -> increase speed
-		else if (isEdgePrimary(edge) && !isEdgeCrowded(edge))
+		// main road but not crowded -> increase speed
+		else if (isEdgeMainRoad(edge) && !isEdgeCrowded(edge))
 			increaseSpeedAtNight = true;
-		// known, not primary, not crowded -> > reroute or increase speed
+		// known, not main road, not crowded -> > reroute or increase speed
 		else if (isEdgeKnown(edge) && !isEdgeCrowded(edge))
 			rerouteOrIncreaseSpeed();
 	}
 
+	/**
+	 * Determines whether to reroute the agent or increase its speed.
+	 */
 	private void rerouteOrIncreaseSpeed() {
 		if (random.nextDouble() < 0.5 && canReroute())
 			computeAlternativeRoute();
@@ -341,16 +442,27 @@ public class AgentMovement {
 			increaseSpeedAtNight = true;
 	}
 
+	/**
+	 * Checks whether the edge is overcrowded based on the agent count.
+	 *
+	 * @param edge The edge to check.
+	 * @return true if the edge is overcrowded, false otherwise.
+	 */
 	private boolean isEdgeCrowded(EdgeGraph edge) {
-
-		double volumePercentile = calculateVolumesPercentile(40);
+		double volumePercentile = calculateVolumesPercentile(20);
 		return edge.getAgentCount() >= volumePercentile;
 	}
 
+	/**
+	 * Calculates the volume percentile for a given percentile.
+	 *
+	 * @param percentile The percentile to calculate.
+	 * @return The volume at the given percentile.
+	 */
 	private double calculateVolumesPercentile(int percentile) {
 		// Collect volumes from edges (Set to List)
-		List<Integer> volumes = PedSimCity.edges.stream().map(EdgeGraph::getAgentCount) // Map each edge to its agent
-																						// count
+		List<Integer> volumes = PedSimCity.edges.stream().map(EdgeGraph::getAgentCount) // Map each edge to its
+																						// agentCount
 				.filter(agentCount -> agentCount > 0) // Only keep agent counts greater than 0
 				.sorted() // Sort the agent counts
 				.collect(Collectors.toList()); // Collect to a List
@@ -367,46 +479,74 @@ public class AgentMovement {
 			return Double.MAX_VALUE;
 	}
 
-	private boolean isEdgePrimary(EdgeGraph edge) {
-		return CommunityCognitiveMap.communityKnownEdges.contains(edge);
+	/**
+	 * Checks if the edge is a main road.
+	 *
+	 * @param edge The edge to check.
+	 * @return true if the edge is primary (or secondary, tertiary, if included), false otherwise.
+	 */
+	private boolean isEdgeMainRoad(EdgeGraph edge) {
+		return CommunityCognitiveMap.getCommunityKnownEdges().contains(edge);
 	}
 
+	/**
+	 * Checks if the agent can reroute based on the current edge and destination.
+	 *
+	 * @return true if the agent can reroute, false otherwise.
+	 */
 	private boolean canReroute() {
 
 		if (currentEdge.getNodes().contains(agent.destinationNode) || indexOnSequence == 0 || !originalRoute)
 			return false;
 		return true;
-
 	}
 
+	/**
+	 * Checks if the edge is known by the agent.
+	 *
+	 * @param edge The edge to check.
+	 * @return true if the edge is known by the agent, false otherwise.
+	 */
 	private boolean isEdgeKnown(EdgeGraph edge) {
 		return agent.getCognitiveMap().isEdgeKnown(edge);
 	}
 
+	/**
+	 * Checks if the edge is next to a park or water.
+	 *
+	 * @param edge The edge to check.
+	 * @return true if the edge is next to a park or water, false otherwise.
+	 */
 	private boolean isEdgeNextToParkOrWater(EdgeGraph edge) {
-		return CommunityCognitiveMap.edgesWithinParks.contains(edge)
-				|| CommunityCognitiveMap.edgesAlongWater.contains(edge);
+		return CommunityCognitiveMap.getEdgesWithinParksOrAlongWater().contains(edge);
 	}
 
-	private boolean isVulnerable() {
-		return agent.vulnerable.equals(Vulnerable.VULNERABLE);
-	}
-
+	/**
+	 * Updates the counts for the current edge the agent is walking on.
+	 */
 	private void updateCounts() {
-
 		edgesWalkedSoFar.add(currentDirectedEdge);
 		currentEdge.incrementAgentCount();
 		agent.kmWalkedTot += currentEdge.getLength();
 		agent.kmWalkedDay += currentEdge.getLength();
 	}
 
+	/**
+	 * Updates the data related to the agent's ruote to derive pedestrian volumes.
+	 */
 	public void updateData() {
 		agent.route.resetRoute(new ArrayList<>(edgesWalkedSoFar));
 		state.flowHandler.updateFlowsData(agent, agent.route, state.isDark);
 	}
 
+	/**
+	 * Resets the path for the agent to follow a new sequence of directed edges.
+	 *
+	 * @param directedEdgesSequence The new sequence of directed edges.
+	 */
 	private void resetPath(List<DirectedEdge> directedEdgesSequence) {
 
+		avoidParksWater = false;
 		indexOnSequence = 0;
 		this.directedEdgesSequence = directedEdgesSequence;
 		currentDirectedEdge = directedEdgesSequence.get(0);
@@ -418,5 +558,4 @@ public class AgentMovement {
 		edgesToAvoid.clear();
 		agent.updateAgentPosition(currentNode.getCoordinate());
 	}
-
 }
