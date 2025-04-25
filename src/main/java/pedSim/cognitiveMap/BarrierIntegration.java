e563c8be4054bb47b2a3466fc74d2ee28de15dfc
Gabriele Filomena <gfilo@liverpool.ac.uk>
2025-04-25 15:32:14 +0100

    Update BarrierIntegration.java

--- src/main/java/pedSim/cognitiveMap/BarrierIntegration.java
+++ src/main/java/pedSim/cognitiveMap/BarrierIntegration.java
@@ -10,16 +10,13 @@ import sim.util.geo.AttributeValue;
 public class BarrierIntegration {
 
 	/**
-	 * Sets the barrier information for an EdgeGraph based on attribute values. This
-	 * method parses attribute strings representing different types of barriers,
-	 * such as positive barriers, negative barriers, rivers, and parks, and
-	 * populates the corresponding lists in the EdgeGraph. The method retrieves
-	 * attribute values for positive barriers ("p_barr"), negative barriers
-	 * ("n_barr"), rivers ("a_rivers"), and parks ("w_parks") from the EdgeGraph's
-	 * attributes. It parses these strings to extract barrier IDs and adds them to
-	 * the appropriate lists: positiveBarriers, negativeBarriers, waterBodies, and
-	 * parks. Additionally, it combines positive and negative barriers into the
-	 * 'barriers' list for convenient access.
+	 * Sets the barrier information for an EdgeGraph based on attribute values. This method parses attribute strings
+	 * representing different types of barriers, such as positive barriers, negative barriers, rivers, and parks, and
+	 * populates the corresponding lists in the EdgeGraph. The method retrieves attribute values for positive barriers
+	 * ("p_barr"), negative barriers ("n_barr"), rivers ("a_rivers"), and parks ("w_parks") from the EdgeGraph's
+	 * attributes. It parses these strings to extract barrier IDs and adds them to the appropriate lists:
+	 * positiveBarriers, negativeBarriers, waterBodies, and parks. Additionally, it combines positive and negative
+	 * barriers into the 'barriers' list for convenient access.
 	 *
 	 * @param edge The EdgeGraph for which barrier information is being set.
 	 */
@@ -75,7 +72,7 @@ public class BarrierIntegration {
 	/**
 	 * It stores information about the barriers within a given SubGraph.
 	 *
-	 * @param subGraph The SubGraph for which the barrier information is being set.
+	 * @param region The Region for which the barrier information is being set.
 	 */
 	public static void setRegionBarriers(Region region) {
 
